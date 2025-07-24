1d71b48f1d9ea0f32f33c7cdb98b774deaf1a795
vishv.patel <vishv.patel@esparkbizmail.com>
2025-07-24 15:30:07 +0530

    fixed bugs And erros

--- src/comment/service/find-all-comment.service.ts
+++ src/comment/service/find-all-comment.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CommentRepo } from '../repositories/comment.repo';
 import { CommentMainDto } from '../dto/comment-main.dto';
+import { EOrder } from 'src/shared/filtering';
 
 @Injectable()
 export class FindAllCommentService {
     constructor(private readonly commentRepo: CommentRepo) {}
     async findAll(): Promise<CommentMainDto[]> {
-        return this.commentRepo.allAsync({});
+        return this.commentRepo.allAsync({$orderBy:'id', $order:EOrder.Desc});
       }
 }
